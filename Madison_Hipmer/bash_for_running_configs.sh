a40961298f593ec03a1480c271f68d81bc2af8bd
madiedunlap <mdunlap@lbl.gov>
2017-06-20 16:25:23 -0700

    Organized into folders Added new edit to config script

--- Madison_Hipmer/bash_for_running_configs.sh
+++ Madison_Hipmer/bash_for_running_configs.sh
@@ -19,3 +19,14 @@ do
         # HIPMER_CONFIG=${file}.config sbatch --nodes=4 ~/hipmeraculous/.edison_deploy/run_hipmer-edison.sh
 done
 
+
+# edit round 2: remove
+
+#!/usr/bin/env bash
+
+for file in *.fastq
+do
+        touch ${file}.config
+        printf "lib_seq %s AlbAL2D 241 48 0 0 0 1 1 1 0 0 1 1\ngenome_size 272000000\nmer_sizes 101\nis_diploid 0\nnum_prefix_blocks 16\nmin_depth_cutoff 2" "$file" | cat - >> ${file}.config
+        # HIPMER_CONFIG=${file}.config sbatch --nodes=4 ~/hipmeraculous/.edison_deploy/run_hipmer-edison.sh
+done
