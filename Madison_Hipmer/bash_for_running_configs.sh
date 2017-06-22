e288c1567565926a5f8b81e76ffa26f58cc25349
madiedunlap <mdunlap@lbl.gov>
2017-06-22 12:33:59 -0700

    edited config script

--- Madison_Hipmer/bash_for_running_configs.sh
+++ Madison_Hipmer/bash_for_running_configs.sh
@@ -41,3 +41,5 @@ do
         printf "lib_seq %s AlbAL2D 535 131 0 0 0 1 1 1 0 0 1 1\nmer_sizes 101\ndynamic_min_depth 0.9\nis_diploid 0\nmin_depth_cutoff 2" "$file" | cat - >> ${file}.config
         # HIPMER_CONFIG=${file}.config sbatch --nodes=4 ~/hipmeraculous/.edison_deploy/run_hipmer-edison.sh
 done
+
+# edit round 4:
