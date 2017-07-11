675c96543855df6294a3d7c3b5525d63e0382c4e
madiedunlap <mdunlap@lbl.gov>
2017-07-11 16:15:16 -0700

    add 3 more parameter tests

--- Madison_Hipmer/bash_for_running_configs.sh
+++ Madison_Hipmer/bash_for_running_configs.sh
@@ -245,14 +245,13 @@ do
 done
 
 
-# edit round 19: range mer_size 71,101,141, try each min_depth
+# edit round 19: range mer_size 57,87,127, try each min_depth
 
 #!/usr/bin/env bash
 
 for file in *.fastq
 do
-        touch ${file}.config
-        printf "lib_seq %s AlbAL2D 535 131 0 0 0 1 1 1 0 0 1 1\nmer_sizes 71,101,141\ndynamic_min_depth 0.9\nis_diploid 0\nmin_depth_cutoff 2" "$file" | cat - >> ${file}.config
-        # HIPMER_CONFIG=${file}.config sbatch --nodes=4 ~/hipmeraculous/.edison_deploy/run_hipmer-edison.sh
+        printf "lib_seq %s AlbAL2D 535 131 0 0 0 1 1 1 0 0 1 1\nmer_sizes 57,87,127\ndynamic_min_depth 0.9\nis_diploid 0\nmin_depth_cutoff 2" "$file" | cat - >> ${file}.config
+        # HIPMER_CONFIG=${file}.config salloc --nodes=8 ~/hipmeraculous/.edison_deploy/run_hipmer-edison.sh
 done
 
